b173bbfbf96713039033d80a4deb7dc96d354798
Jonathan <111000657+MrGuato@users.noreply.github.com>
2026-01-06 04:23:03 +0000

    feat; ship pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mrguato.github.io",
+  base: "/gitops-notes",
+});
